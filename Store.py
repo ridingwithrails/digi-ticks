6f0e668e1f2f52af32c4c56ae6d4ff966059fa7c
ridingwithrails <ridingwithrails@gmail.com>
2021-11-16 01:42:32 +0000

    Adding changes for user

--- Store.py
+++ Store.py
@@ -1,7 +1,24 @@
 from User import User
 from Product import Product
+from getpass import getpass
+
 
 data = ''
+guest = True
+
+while guest:
+    username = input("Please type in username:\n")
+    password = getpass()
+    print(password)
+
+    users = User.build()
+    print(users)
+
+    guest = False
+
+
+
+
 while data != 'E':
     data = input("Store has started.  [L] List Products  [E] Exit\n")
     if data == 'L':
