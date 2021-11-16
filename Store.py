e2afa92a999a8d3df60a77edbce22a007c98ce15
DoniTay <soccerdonovant@gmail.com>
2021-11-15 18:45:33 -0800

    adding a user promt

--- Store.py
+++ Store.py
@@ -1,8 +1,33 @@
 from User import User
 from Product import Product
+from getpass import getpass
 
 data = ''
+guest = True
+users = User.build()
+
+def is_user(username, password):
+     print(username)
+     print(password)
+     if username in users:
+            user = users(username)
+            return user.password == password
+     else:
+        return True
+
+while guest:
+    username = input("Please type in username:\n")
+    password = getpass()
+    if is_user(username, password):
+        guest = False
+    else:
+        guest = True
+
+
+
 while data != 'E':
     data = input("Store has started.  [L] List Products  [E] Exit\n")
     if data == 'L':
         Product.list()
+
+ 
